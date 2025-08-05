b10f2d98a15bae7517c4d7b2aba39f2e8d3ecac4
wildanzm <wildanmanapi123@gmail.com>
2025-08-05 20:06:37 +0700

    fix: routes and session

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,24 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Volt\Volt;
 use App\Http\Controllers\PdfController;
 
 Route::get('/', function () {
-    return view('welcome');
-})->name('home');
-
-Route::redirect('/', '/login');
+    if (Auth::check()) {
+        return redirect()->route('profile');
+    } else {
+        return redirect()->route('login');
+    }
+});
 
 Route::middleware(['auth'])->group(function () {
     Volt::route('screening', 'admin.screening')->name('screenings');
     Volt::route('history', 'admin.report')->name('reports');
     Volt::route('profile', 'admin.profile')->name('profile');
     Volt::route('/screening/result/{screening}', 'admin.result')->name('screening.result');
-    
+
     // PDF Download Routes
     Route::get('/screening/pdf/{screening}', [PdfController::class, 'downloadScreeningReport'])
         ->name('screening.pdf');
